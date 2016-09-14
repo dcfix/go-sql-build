4cac98daf40f74c478c229ae1bb7be4d14728f04
Darren Fix <darren.fix@gmail.com>
2016-09-14 06:55:31 -0600

    Added flag to process single directory

--- gobolt.go
+++ gobolt.go
@@ -20,10 +20,16 @@ type Config struct {
 func main() {
 	// see if they passed in a config.json file,
 	// if not, see if there is one in the local directory.
-        configPath := flag.String("configPath", "gobolt.config.json", "the config file to process")
+        configPath := flag.String("configPath", "gobolt.config.json", "The config file to process.")
+        section := flag.String("section", "all", "The section to run.")
         flag.Parse()
 
 	config := loadConfig(*configPath)
+        if *section != "all" {
+          newDirStructure := make([]string,1)
+          newDirStructure[0] = *section
+          config.DirectoryStructure = newDirStructure
+        }
 	processFiles(config)
 }
 
@@ -35,9 +41,9 @@ func processFiles(config Config) {
 		//fmt.Println(path)
 
 		err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
-                  fmt.Println(path)
+                  // fmt.Println(path)
                   if strings.HasSuffix(path, ".sql") {
-                    fmt.Printf(" processing %v\n", path)
+                    fmt.Printf("  processing %v\n", path)
                     args := append(config.CommandArgs, path)
                     runSql(config.Command, args)
                 }
@@ -48,7 +54,6 @@ func processFiles(config Config) {
 		}
 	}
 }
-
 
 func loadConfig(configPath string) Config {
 	file, _ := os.Open(configPath)
@@ -65,7 +70,6 @@ func loadConfig(configPath string) Config {
 func runSql(exe string, args []string) string {
 
 	cmd := exec.Command(exe, args...)
-
 	stdout, err := cmd.CombinedOutput()
 
 	if err != nil {
